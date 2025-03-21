bc7ab2d2489f105fd8183ffce0e2babe5255c069
Saif <a2409980@institutmontilivi.cat>
2025-03-21 15:26:44 +0100

    Update Nau.py

--- Nau.py
+++ Nau.py
@@ -17,16 +17,16 @@ def main():
         op = input('Entra una opció')
         if op=='d':
             #sumar 1 a la variable posX
-            pass
+            posX=posX+1
         elif op=='a':
             #restar 1 a la variable posX
-            pass
+            posX=posX-1
         elif op=='w':
             #sumar 1 a la variable posY
-            pass
+            posY=posY+1
         elif op=='s':
             #restar 1 a la variable posY
-            pass
+            posY=posY-1
         elif op=='0':
             sortir=True
             print("Has sortit de la nau")
